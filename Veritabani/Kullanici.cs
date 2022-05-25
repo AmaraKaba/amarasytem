22e3fd131af8fc97e8dc1b136a91e5fe1c023301
AmaraKaba <82162210+AmaraKaba@users.noreply.github.com>
2022-05-25 16:38:34 +0300

    Veritaba_Ekleme

---
+++ Veritabani/Kullanici.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veritabani
+{
+    class Kullanici
+    {
+        public partial class Kullanci
+        {
+            public int Kullanci_id { get; set; }
+            public string Kullanci_adi { get; set; }
+            public string Kullanci_email { get; set; }
+            public string Kullanci_TC { get; set; }
+            public string Kullanci_sifresi { get; set; }
+            public string Kullanci_Yetki { get; set; }
+          
+            
+        }
+    }
+}
---
+++ Veritabani/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veritabani
+{
+    public partial class Menu
+    {
+        public int Menu_id { get; set; }
+        public string Menu_ad { get; set; }
+        public Nullable<int> Menu_sira { get; set; }
+        public string Menu_yetki { get; set; }
+        public string Menu_adres { get; set; }
+       
+    }
+}
